eb8abb6feda75e7bca76483cb29f5d0f32f03280
Vataz1337 <77028131+Vataz1337@users.noreply.github.com>
2022-01-20 11:11:49 +0100

    Initial Commit

---
+++ src/test/java/com/aplication/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
